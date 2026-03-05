7397ea03a55b3d0112e73ba1048d78bbf837c149
Gourav-Coder-01 <gourav.kr201@gmail.com>
2026-03-05 17:14:47 +0530

    updated

--- frontend-part/src/pages/Collection.jsx
+++ frontend-part/src/pages/Collection.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../context/ShopContext";
+import { ShopContext } from "../context/Shopcontext";
 import { assets } from "../assets/assets";
 import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
--- frontend-part/src/pages/Login.jsx
+++ frontend-part/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext';
+import { ShopContext } from '../context/Shopcontext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
